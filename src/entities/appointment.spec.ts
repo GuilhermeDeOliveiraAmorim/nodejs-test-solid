d1f8241b663e9025b8f9cf3bb271a3ccd2e9d499
GuilhermeDeOliveiraAmorim <guilherme.o.a.ufal@gmail.com>
2022-09-10 12:05:31 -0300

    feat: add test for ends date smaller than starts date

--- src/entities/appointment.spec.ts
+++ src/entities/appointment.spec.ts
@@ -16,3 +16,18 @@ test("add appointment", () => {
     expect(appointment).toBeInstanceOf(Appointment);
     expect(appointment.customer).toEqual("A");
 });
+
+test("cannot create an appointment with ends date smaller than starts date", () => {
+    const startsAt = new Date();
+    const endsAt = new Date();
+
+    endsAt.setDate(endsAt.getDate() - 1);
+
+    expect(() => {
+        return new Appointment({
+            customer: "A",
+            startsAt,
+            endsAt,
+        });
+    }).toThrow();
+});
