83e5533045c081777ea087f22d5fb2bba5c8f22c
GuilhermeDeOliveiraAmorim <guilherme.o.a.ufal@gmail.com>
2022-09-10 13:33:19 -0300

    feat: add repository for testing

--- src/use-cases/create-appointment.spec.ts
+++ src/use-cases/create-appointment.spec.ts
@@ -1,11 +1,13 @@
 import { describe, expect, it } from "vitest";
 import { Appointment } from "../entities/appointment";
+import { InMemoryAppointmentsReporitory } from "../repositories/in-memory/in-memory-appointments-reporitory";
 import { getFutureDate } from "../tests/utils/get-future-date";
 import { CreateAppointment } from "./create-appointment";
 
 describe("Create Appointment", () => {
     it("should be able to create an appointment", () => {
-        const createAppointment = new CreateAppointment();
+        const appointmentsRepository = new InMemoryAppointmentsReporitory();
+        const createAppointment = new CreateAppointment(appointmentsRepository);
 
         const startsAt = getFutureDate("2022-08-10");
         const endsAt = getFutureDate("2022-08-11");
